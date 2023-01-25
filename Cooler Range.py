685828b8ac495db15f7bbb4738b4a862becbd425
PureSkillz_BG <81581754+PureSkillz@users.noreply.github.com>
2023-01-25 14:03:56 +0200

    Update Cooler Range.py

--- Cooler Range.py
+++ Cooler Range.py
@@ -2,13 +2,13 @@ def cooler_range(first, *args, **kwargs):
     answer = [i for i in range(first)]
     try:
         answer = [i for i in range(first, args[0])]
-        answer = [i for i in range(first, args[0], args[1])]
-        answer = [el for el in answer if el not in args[2]]  # could use kwargs[skip]
+        answer = [i for i in range(first, args[0], kwargs["step"])]
+        answer = [el for el in answer if el not in kwargs["skip"]]
     finally:
         return answer
 
 
 print(cooler_range(5))
 print(cooler_range(3, 10))
-print(cooler_range(0, 10, 2))
-print(cooler_range(0, 10, 1, [1, 5, 7]))
+print(cooler_range(0, 10, step=2))
+print(cooler_range(0, 10, step=1, skip=[1, 5, 7]))
